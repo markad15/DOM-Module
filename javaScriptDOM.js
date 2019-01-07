44ead65cb9bc40bafac275c273d6bff8c06c42ed
Ankush <markad.ankush@yahoo.com>
2019-01-07 13:51:06 +0530

    Save Day1

--- javaScriptDOM.js
+++ javaScriptDOM.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMContentLoaded",function(){
 
 function addStudent()
 {
-    var studName = document.getElementById("stud-name-id").Value;
-    var studNumber = document.getElementById("stud-number-id").Value;
-    var studEmail = document.getElementById("stud-email-id").Value;
-    var studRemove = "Remove";
+    event.preventDefault(); 
+    var studName = document.getElementById("stud-name-id").value;
+    var studNumber = document.getElementById("stud-number-id").value;
+    var studEmail = document.getElementById("stud-email-id").value;
+    // var studRemove = "Remove";
+
+    console.log(studName,studNumber,studEmail);
 
     var trStud = document.createElement("tr");
     var tdStudName = document.createElement("td");
@@ -13,21 +15,31 @@ function addStudent()
     var tdStudEmail = document.createElement("td");
     var tdStudremove = document.createElement("td");
 
+    var tablestud = document.querySelector("table");
+
     trStud.className = "tr-stud-info";
     tdStudName.className = "td-stud-name";
     tdStudNumber.className = "td-stud-number";
     tdStudEmail.className = "td-stud-email";
-    tdStudremove.className = "td-stud-remove";
+    tdStudremove.className = "td-stud-remove";    
     
-    var insertStudDetails = 
-    "<td class='td-stud-name'>" +studName+ "</td>"
-    "<td class='td-stud-number'>" +studNumber+ "</td>"
-    "<td class='td-stud-email'>" +studEmail+ "</td>"
-    "<td class='td-stud-remove'>" + studRemove + "</td>"
 
-    trStud.append(insertStudDetails);
-}
+    //  var insertStudDetails = 
+    // "<tr class='tr-stud-info'><td class='td-stud-name'>" +studName+ "</td>"
+    // "<td class='td-stud-number'>" +studNumber+ "</td>"
+    // "<td class='td-stud-email'>" +studEmail+ "</td>"
+    // "<td class='td-stud-remove'>" + studRemove + "</td> </tr>"
 
+     trStud.append(tdStudName);
+     trStud.append(tdStudNumber);
+     trStud.append(tdStudEmail);
+     trStud.append(tdStudremove);
 
+    tablestud.append(trStud);
 
-})
+     tdStudName.innerText = studName;
+     tdStudNumber.innerText = studNumber;
+     tdStudEmail.innerText = studEmail;
+     tdStudremove.innerText = "Remove"
+
+};
